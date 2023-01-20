e73f3dc508651bc02f2df5b6026bcf824372c139
array <bakirova0993@gmail.com>
2023-01-20 17:54:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle();
+
+        System.out.println("Area = " + Circle.area(20.0));
+        System.out.println("Circumference = " + Circle.circumference(15.5));
+
+    }
+}
